8f971c8edbf4e5d7c91e41e4a3eb0f63a00628c1
Fatima Bitar <fatimabitar97@gmail.com>
2021-12-24 11:23:40 +0200

    contact section

--- js/navbar.js
+++ js/navbar.js
@@ -4,38 +4,45 @@ let navbar = document.querySelector(".navbar");
 const links = document.querySelectorAll(".nav-link");
 const sections = document.querySelectorAll(".section");
 
-onscroll = function () {
+// onscroll = function () {
+//   if (window.pageYOffset > 500) {
+//     navbar.classList.add("bg-dark");
+//   } else {
+//     navbar.classList.remove("bg-dark");
+//   }
+//   var scrollPosition = document.documentElement.scrollTop;
+
+//   sections.forEach((section) => {
+//     if (
+//       scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
+//       scrollPosition <
+//         section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
+//     ) {
+//       var currentId = section.attributes.id.value;
+//       removeAllActiveClasses();
+//       addActiveClass(currentId);
+//     }
+//   });
+// };
+
+// var removeAllActiveClasses = function () {
+//   document.querySelectorAll("nav a").forEach((el) => {
+//     el.classList.remove("active");
+//   });
+// };
+
+// var addActiveClass = function (id) {
+//   // console.log(id);
+//   var selector = `nav a[href="#${id}"]`;
+//   document.querySelector(selector).classList.add("active");
+// };
+onscroll = function(){
   if (window.pageYOffset > 500) {
     navbar.classList.add("bg-dark");
   } else {
     navbar.classList.remove("bg-dark");
   }
-  var scrollPosition = document.documentElement.scrollTop;
-
-  sections.forEach((section) => {
-    if (
-      scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
-      scrollPosition <
-        section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
-    ) {
-      var currentId = section.attributes.id.value;
-      removeAllActiveClasses();
-      addActiveClass(currentId);
-    }
-  });
-};
-
-var removeAllActiveClasses = function () {
-  document.querySelectorAll("nav a").forEach((el) => {
-    el.classList.remove("active");
-  });
-};
-
-var addActiveClass = function (id) {
-  // console.log(id);
-  var selector = `nav a[href="#${id}"]`;
-  document.querySelector(selector).classList.add("active");
-};
+}
 
 links.forEach((link) => {
   link.addEventListener("click", (e) => {
