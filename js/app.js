8eae8a167b634becfcf04e98089c8d07ec6f0fab
Fatima Bitar <fatimabitar97@gmail.com>
2021-12-24 02:12:31 +0200

    contact section

--- js/app.js
+++ js/app.js
@@ -3,3 +3,27 @@
 // for (let i = 0; i < svgText.length; i++) {
 //   console.log(`Letter ${i} is ${svgText[i].getTotalLength()}`+"555555");
 // }
+
+
+//back to top
+let backToTop = document.getElementById("toTopButton");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 500) {
+    //Show Back To Top button
+    backToTop.style.transform = "scale(1)";
+  } else {
+    // Hide Back To Top button
+    backToTop.style.transform = "scale(0)";
+  }
+}
+
+backToTop.onclick = () => {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,51 @@
 let navbar = document.querySelector(".navbar");
 
-window.onscroll = () => {
+/* Toggle active class */
+const links = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll(".section");
+
+onscroll = function () {
   if (window.pageYOffset > 500) {
     navbar.classList.add("bg-dark");
   } else {
     navbar.classList.remove("bg-dark");
   }
+  var scrollPosition = document.documentElement.scrollTop;
+
+  sections.forEach((section) => {
+    if (
+      scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
+      scrollPosition <
+        section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
+    ) {
+      var currentId = section.attributes.id.value;
+      removeAllActiveClasses();
+      addActiveClass(currentId);
+    }
+  });
 };
+
+var removeAllActiveClasses = function () {
+  document.querySelectorAll("nav a").forEach((el) => {
+    el.classList.remove("active");
+  });
+};
+
+var addActiveClass = function (id) {
+  // console.log(id);
+  var selector = `nav a[href="#${id}"]`;
+  document.querySelector(selector).classList.add("active");
+};
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    var currentId = e.target.attributes.href.value;
+    var section = document.querySelector(currentId);
+    var sectionPos = section.offsetTop;
+    window.scroll({
+      top: sectionPos,
+      behavior: "smooth",
+    });
+  });
+});
