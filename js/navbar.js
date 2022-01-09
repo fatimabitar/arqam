58021d79ca30773e8bc92cc39e2b13286f562864
Fatima Bitar <fatimabitar97@gmail.com>
2022-01-09 12:37:03 +0200

    no rect

--- js/navbar.js
+++ js/navbar.js
@@ -4,38 +4,6 @@ let navbar = document.querySelector(".navbar");
 const links = document.querySelectorAll(".nav-link");
 const sections = document.querySelectorAll(".section");
 
-// onscroll = function () {
-//   if (window.pageYOffset > 500) {
-//     navbar.classList.add("bg-dark");
-//   } else {
-//     navbar.classList.remove("bg-dark");
-//   }
-//   var scrollPosition = document.documentElement.scrollTop;
-
-//   sections.forEach((section) => {
-//     if (
-//       scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
-//       scrollPosition <
-//         section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
-//     ) {
-//       var currentId = section.attributes.id.value;
-//       removeAllActiveClasses();
-//       addActiveClass(currentId);
-//     }
-//   });
-// };
-
-// var removeAllActiveClasses = function () {
-//   document.querySelectorAll("nav-links").forEach((el) => {
-//     el.classList.remove("active");
-//   });
-// };
-
-// var addActiveClass = function (id) {
-//   // console.log(id);
-//   var selector = `links[href="#${id}"]`;
-//   selector.classList.add("active");
-// };
 onscroll = function () {
   if (window.pageYOffset > 500) {
     navbar.classList.add("bg-dark");
@@ -43,16 +11,3 @@ onscroll = function () {
     navbar.classList.remove("bg-dark");
   }
 };
-
-links.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    var currentId = e.target.attributes.href.value;
-    var section = document.querySelector(currentId);
-    var sectionPos = section.offsetTop;
-    window.scroll({
-      top: sectionPos,
-      behavior: "smooth",
-    });
-  });
-});
